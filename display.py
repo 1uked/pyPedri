69aa2ce95104d328cfb7d4d9eadf73f7fb44e985
1uked <123551653+1uked@users.noreply.github.com>
2023-10-29 19:48:48 +0000

    A

--- display.py
+++ display.py
@@ -1,22 +1,11 @@
 import pygame
-import minesweeper
+import minesweeper, button
 
-sweeper = minesweeper.Minesweeper(10, 10, 10)
+sweeper = minesweeper.Minesweeper(20, 20, 50)
 
 pygame.init()
 
 # create game window
-
-# arr = [['Z', 'X', 'Z', 'Z', 'Z', 'X', 'Z', 'Z', 'Z', 'Z'],
-#        ['Z', 'X', 'Z', 'Z', 'Z', 'X', 'Z', 'Z', 'Z', 'Z'],
-#        ['Z', 'X', '1', '2', 'Z', 'X', 'Z', 'Z', 'Z', 'Z'],
-#        ['Z', 'X', 'Z', 'Z', 'Z', 'X', 'Z', 'Z', 'Z', 'Z'],
-#        ['Z', 'X', 'Z', 'Z', 'Z', 'X', 'Z', 'Z', 'Z', 'Z'],
-#        ['Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z', 'Z'],
-#        ['Z', 'X', 'Z', 'Z', 'Z', 'X', 'Z', 'Z', '3', 'Z'],
-#        ['Z', 'X', 'Z', 'Z', 'Z', 'X', 'Z', 'Z', 'Z', 'Z'],
-#        ['Z', 'Z', 'Z', 'Z', '8', 'X', 'Z', 'Z', 'Z', 'Z'],
-#        ['1', '2', '3', '4', '5', '6', '7', '8', 'Z', 'Z']]
 
 TILES = len(sweeper.board)
 
@@ -44,7 +33,7 @@ for i in range(0, 9):
 tile_bomb = pygame.image.load("Data/Tiles/tile_mine.png")
 tile_plain = pygame.image.load("Data/Tiles/tile_plain2.png")
 tile_wrong = pygame.image.load("Data/Tiles/tile_wrong.png")
-tile_flag = pygame.image.load("Data/Tiles/tile_flag.png")
+tile_flag = pygame.image.load("Data/Tiles/tile_flag_.png")
 
 tile_bomb = pygame.transform.scale(tile_bomb, (TILE_WIDTH, TILE_HEIGHT))
 tile_plain = pygame.transform.scale(tile_plain, (TILE_WIDTH, TILE_HEIGHT))
@@ -68,7 +57,7 @@ def action(x, y):
         if ybuffer < y < SCREEN_HEIGHT - ybuffer:
             x = (x - xbuffer) // TILE_WIDTH
             y = (y - ybuffer) // TILE_HEIGHT
-            sweeper.set_inputs(int(y),int(x))
+            sweeper.set_inputs(int(y), int(x))
 
 
 def display(arr):
@@ -89,9 +78,6 @@ def display(arr):
 
 while running:
     screen.blit(bg, (0, 0))
-    pygame.draw.rect(screen, "GREY",
-                     pygame.Rect((SCREEN_WIDTH - BOARD_WIDTH) // 2 - 10, (SCREEN_HEIGHT - BOARD_HEIGHT) // 2 - 10,
-                                 BOARD_WIDTH + 20, BOARD_HEIGHT + 20))
 
     display(sweeper.board)
 
@@ -100,7 +86,6 @@ while running:
             running = False
         if event.type == pygame.MOUSEBUTTONUP:
             action(event.pos[0], event.pos[1])
-
 
     pygame.display.flip()
     clock.tick(60)
