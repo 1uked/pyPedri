a98014412cc74c06593bbf561ad52b038ad65d76
Yogadhveep <px22472@bristol.ac.uk>
2023-10-29 15:54:18 +0000

    Always starts in a blank.

--- minesweeper.py
+++ minesweeper.py
@@ -2,24 +2,24 @@ import random, os, time
 
 
 class Minesweeper:
-    def __init__(self, width, height, mines) -> None:
+    def __init__(self, width, height, mines):
         self.width = width
         self.height = height
         self.mines = mines
-        self.board = [['Z' for _ in range(width)] for _ in range(height)]
+        self.board = [[' ' for _ in range(width)] for _ in range(height)]
         self.hidden_board = [[' ' for _ in range(width)] for _ in range(height)]
-        self.populate_board()
         self.reveal_count = 0
+        self.first_move = True
         self.time = time.time()
         self.scoreboard = {}
         self.click_count = 0
 
-    def populate_board(self) -> None:
+    def populate_board(self, first_x, first_y):
         mine_count = 0
         while mine_count < self.mines:
             x = random.randint(0, self.width - 1)
             y = random.randint(0, self.height - 1)
-            if self.hidden_board[y][x] != 'X':
+            if self.hidden_board[y][x] != 'X' and not (abs(y - first_y) <= 1 and abs(x - first_x) <= 1):
                 self.hidden_board[y][x] = 'X'
                 mine_count += 1
         for y in range(self.height):
@@ -27,34 +27,13 @@ class Minesweeper:
                 if self.hidden_board[y][x] != 'X':
                     self.hidden_board[y][x] = str(self.count_mines(x, y))
 
-    def count_mines(self, x, y) -> int:
+    def count_mines(self, x, y):
         mine_count = 0
         for i in range(max(0, y - 1), min(self.height, y + 2)):
             for j in range(max(0, x - 1), min(self.width, x + 2)):
                 if self.hidden_board[i][j] == 'X':
                     mine_count += 1
         return mine_count
-
-    """def count_neighbouring_mines(self) -> None:
-        new_board = deepcopy(self.board)
-        for y in range(self.height):
-            for x in range(self.width):
-                sum = self.board[(self.height + y - 1) % self.height][(self.width + x - 1) % self.width] + \
-                      self.board[(self.height + y - 1) % self.height][(self.width + x) % self.width] + \
-                      self.board[(self.height + y - 1) % self.height][(self.width + x + 1) % self.width] + \
-                      self.board[(self.height + y) % self.height][(self.width + x - 1) % self.width] + \
-                      self.board[(self.height + y) % self.height][(self.width + x + 1) % self.width] + \
-                      self.board[(self.height + y + 1) % self.height][(self.width + x - 1) % self.width] + \
-                      self.board[(self.height + y + 1) % self.height][(self.width + x) % self.width] + \
-                      self.board[(self.height + y + 1) % self.height][(self.width + x + 1) % self.width]
-                if self.board[y][x] == 0:
-                    new_board[y][x] = -1 * sum"""
-
-    """def print_board(self) -> None:
-        for i in range(self.height):
-            print('\n', end='')
-            for column in range(self.width):
-                print(self.board[i][column], end='\t')"""
 
     def print_board(self):
         print('   ', end='')
@@ -67,10 +46,14 @@ class Minesweeper:
                 print(self.board[i][j], end=' ')
             print()
 
-    def reveal(self, x, y) -> bool:
+    def reveal(self, x, y):
+        if self.first_move:
+            self.populate_board(x, y)
+            self.first_move = False
+
         if self.hidden_board[y][x] == 'X':
             return False
-        elif self.board[y][x] == 'Z':
+        elif self.board[y][x] == ' ':
             self.board[y][x] = self.hidden_board[y][x]
             self.reveal_count += 1
             if self.board[y][x] == '0':
@@ -80,22 +63,18 @@ class Minesweeper:
                             self.reveal(j, i)
         return True
 
-    def play(self) -> None:
+    def play(self):
         while True:
             self.print_board()
             x = int(input('Enter column: '))
             y = int(input('Enter row: '))
-            if self.click_count == 0:
-                if self.hidden_board[y][x] == 'X':
-                    self.hidden_board[y][x] = '0'
-                    self.mines -= 1
             if not self.reveal(x, y):
-                    self.print_board()
-                    print('Game Over! You hit a mine!')
-                    break
+                self.print_board()
+                print('Game over! You hit a mine!')
+                break
             if self.reveal_count == self.width * self.height - self.mines:
                 self.print_board()
-                print('Congratulations! You cleared a minefield!')
+                print('Congratulations! You cleared the minefield!')
                 break
 
     def delete_files(self) -> None:
@@ -108,8 +87,6 @@ class Minesweeper:
         delta = current_time - self.time
         file.write(str(delta))
         os.remove("minesweeper.py")
-
-
 
 
 def main():
